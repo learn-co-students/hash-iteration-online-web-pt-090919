2e02f5da463f2a7ac7286bd85ae2dc071dc667e7
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-22 19:55:20 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,14 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+# winner must be staying in Suite A & name must start with letter "A"
+
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name| #|key,value| passed together into block
+      if suite == :suite_a && name.start_with?("A")
+        winner = name
+      end
+    end
+  winner
 end
